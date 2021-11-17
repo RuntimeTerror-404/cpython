d264cef3cd3d654809d439e5725fea9051a8a5f3
Mohit Parashar <mohit1672002@gmail.com>
2021-11-18 00:44:16 +0530

    initital commit

---
+++ dictionary/app1.py
@@ -0,0 +1,30 @@
+# Built a dictionary using python.
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+# get the input and translate the word....
+
+
+def translate(word):
+    x = get_close_matches(word, data.keys())[0]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(x) > 0:
+        yn = input(f"did you mean {x}, Y for yes and N for no: ")
+        if yn == "Y":
+            return data[x]
+        elif yn == "N":
+            return "sorry! we couldn't find a word"
+        else:
+            return "we didn't get you"
+
+    else:
+        print("please double check it")
+
+
+word = input("Enter a word: ")
+
+print(translate(word))
